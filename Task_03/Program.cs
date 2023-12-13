c74d8cd70987cad68de89ec6f475a6a4081fed89
Mr.D <damedetov@mail.rum>
2023-12-13 18:09:39 +0600

    Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void ShowArray(int[]array, int size)
+{
+    if(size>=0)
+    {
+        System.Console.Write(array[size]+" ");
+        ShowArray(array, size-1);
+    }
+}
+int []array ={3, 2, 5, 0, 10, 34};
+ShowArray(array, array.Length-1);
