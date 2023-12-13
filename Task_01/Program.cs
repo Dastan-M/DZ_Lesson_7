7de44be5597d524929eb4e9f3835cf10e356f6b3
Mr.D <damedetov@mail.rum>
2023-12-13 17:02:34 +0600

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void ShowNums (int m, int n)
+{
+    if(m<=n)
+    {
+        System.Console.Write(m+",");
+        ShowNums(m+1, n);
+    }
+}
+ShowNums(m,n);
